ad1f6f233acbf794a1caf4eb0afa5ce80091e9fc
Colin Rundel <rundel@gmail.com>
2026-07-02 11:09:59 -0400

    Configure org before inviting

--- Organization/invite.R
+++ Organization/invite.R
@@ -11,6 +11,12 @@ library(ghclass)
 
 org = "ghclass-paper"
 
+# Configure the organization before anyone is invited
+org_set_repo_permission(org, "none")
+org_set_workflow_permissions(org, "write")
+
+org_sitrep(org)
+
 # In a real course this file holds PII and stays out of version control;
 # here every row is fabricated.
 roster = readr::read_csv("github_roster.csv")
@@ -21,8 +27,3 @@ stopifnot(all(valid))
 
 need = setdiff(responded$github, c(org_members(org), org_pending(org)))
 org_invite(org, need)
-
-org_set_repo_permission(org, "none")
-org_set_workflow_permissions(org, "write")
-
-org_sitrep(org)
